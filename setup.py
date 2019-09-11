e1ea6de02c3109c45752a3325deca7a59917f889
Jonathan Karr <jonrkarr@gmail.com>
2019-09-11 11:04:30 -0400

    updating contact email address

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
-    author_email="karr@mssm.edu",
+    author_email="info@karrlab.org",
     license="MIT",
     keywords='whole-cell computational systems biology docker dependencies',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
